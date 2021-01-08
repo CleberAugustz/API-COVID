2d69c5b48d3a73464cb0722c2617ae67bd89d019
CleberAugustz <cleber.augustz@hotmail.com>
2021-01-08 19:43:56 -0300

    Alter hour to execute

--- src/service/etherealEmailLog.js
+++ src/service/etherealEmailLog.js
@@ -49,9 +49,7 @@ var etherealEmailLog = /** @class */ (function () {
                 console.log("Credentials obtainer, sending message...");
                 transporter = nodemailer_1.default.createTransport({
                     host: process.env.EMAIL_HOST,
-                    ignoreTLS: false,
-                    secure: false,
-                    port: 587,
+                    port: process.env.EMAIL_PORT,
                     auth: {
                         user: process.env.EMAIL_AUTH_USER,
                         pass: process.env.EMAIL_AUTH_PASS,
